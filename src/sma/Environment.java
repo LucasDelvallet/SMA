bf4dac82d83cd45af0a8b2babd530a9d6d27261e
Delvallet Lucas <delvallet@b10p2.fil.univ-lille1.fr>
2017-01-04 17:36:43 +0100

    First Commit

---
+++ src/sma/Environment.java
@@ -0,0 +1,31 @@
+package sma;
+
+import java.util.List;
+
+public class Environment {
+	
+	private int width;
+	private int height;
+	private SMA sma;
+	public List<Agent> agentlist;
+	
+	
+	public Environment(int width, int height){
+		this.width = width;
+		this.height = height;
+		
+		//Creer les agents.
+		
+		//this.sma = new SMA(this, agentlist);
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
+	
+	
+}
