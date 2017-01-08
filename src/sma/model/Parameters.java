4d096a67078c95d178909b52a224ab7c041a8ae8
LucasDelvallet <lucas.delvallet@hotmail.fr>
2017-01-08 20:47:09 +0100

    Updated parameters

--- src/sma/model/Parameters.java
+++ src/sma/model/Parameters.java
@@ -1,8 +1,8 @@
 package sma.model;
 
 public class Parameters {
-	public static final int gridSizeX = 50;
-	public static final int gridSizeY = 50;
+	public static final int gridSizeX = 500;
+	public static final int gridSizeY = 500;
 	public static final int boxSize = 10;
 	public static final int delay = 90;
 	public static final int scheduling = 0; // 0:equitable 1:sequentiel, 2:aléatoire
@@ -11,6 +11,6 @@ public class Parameters {
 	public static final boolean trace = false;
 	public static final int seed = 1;
 	public static final int refresh = 1;
-	public static final int nbParticles = 25;
+	public static final int nbParticles = 10000;
 	public static final boolean toric = false;
 }
